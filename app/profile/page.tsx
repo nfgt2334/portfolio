1544ac2e50b2965a62de623e11aeb04a20a6ff49
f_takeshi <fjk.1122@icloud.com>
2025-04-27 08:27:51 +0900

    アニメーション処理修正

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,6 @@
 import { Metadata } from 'next'
 
-import { ProfileHeader } from './_components/ProfileHeader'
-import { InfoItem } from './_components/InfoItem'
-import { SkillBadge } from './_components/SkillBadge'
-import { AboutMe } from './_components/AboutMe'
+import { ProfileContent } from './_components/ProfileContent'
 
 export const metadata: Metadata = {
   title: 'Works | Portfolio',
@@ -11,14 +8,5 @@ export const metadata: Metadata = {
 }
 
 export default function Profile() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-4 py-12 bg-white text-black dark:bg-gray-900 dark:text-white">
-      <div className="min-h-screen flex flex-col items-center justify-center px-4 py-12 bg-white text-black dark:bg-gray-900 dark:text-white">
-        <ProfileHeader />
-        <InfoItem />
-        <SkillBadge />
-        <AboutMe />
-      </div>
-    </div>
-  )
+  return <ProfileContent />
 }
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,5 +1,4 @@
-import { SkillList } from './_components/SkillList'
-import { EngineerExperienceYear } from './_components/EngineerExperienceYear'
+import { SkillsContent } from './_components/SkillsContent'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -8,30 +7,5 @@ export const metadata: Metadata = {
 }
 
 export default function Skills() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-4 py-12 bg-white text-black dark:bg-gray-900 dark:text-white">
-      <h1 className="font-extrabold text-4xl mb-8 flex justify-center items-center space-x-2">
-        <span>
-          Skills{' '}
-          <span className="text-gray-500 text-sm dark:text-gray-400">
-            技術スタック
-          </span>
-        </span>
-      </h1>
-
-      {/* エンジニア経験年数 */}
-      <div className="mb-8 text-center">
-        <h2 className="text-2xl font-semibold mb-2">Engineer Experience</h2>
-
-        {/* カウントアップ */}
-        <EngineerExperienceYear />
-        <p className="text-lg text-gray-700 dark:text-gray-300 mt-2">
-          日々学び、成長しています！
-        </p>
-      </div>
-
-      {/* Skills */}
-      <SkillList />
-    </div>
-  )
+  return <SkillsContent />
 }
